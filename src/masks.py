57abe5ece0390a93c21087b95d48c940bb27356d
unknown <extapo@yandex>
2024-12-11 23:30:42 +0700

    add src/masks.py

---
+++ src/masks.py
@@ -0,0 +1,8 @@
+def get_mask_card_number(card_number: str) -> str:
+    """ Функция маскирует номер карты, заменяя 7-12 цифры на '*' """
+    return card_number[0:4] + " " + card_number[4:6] + "** ****" + card_number[-4:]
+
+
+def get_mask_account(accaunt_number: str) -> str:
+    """ Функция маскирует номер аккаунта, оставляя последние 4 цифры"""
+    return "**" + accaunt_number[-4:]
