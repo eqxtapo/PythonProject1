bb9f2c9befce07fb9705de694fedc9c90703c9e5
unknown <extapo@yandex>
2025-01-11 00:07:20 +0700

    add decorators with tests

--- src/decorators.py
+++ src/decorators.py
@@ -1,28 +1,28 @@
-from functools import wraps
+import os
 from typing import Any
 
 
 def log(filename: Any = None) -> Any:
     """Декоратор автоматически логирует начало и конец выполнения функции,
     а также ее результаты или возникшие ошибки."""
-    def decorator(my_func: Any) -> Any:
-        @wraps(my_func)
-        def wrapper(*args: Any, **kwargs: Any) -> Any:
-            result = my_func(*args, **kwargs)
-            if not filename:
-                print(f'{my_func.__name__} started')
-                try:
-                    print(f'{my_func.__name__} ok')
-                    print(f'{my_func.__name__} finished')
-                except Exception as e:
-                    print(f'{my_func.__name__} error: {e}. Inputs: {args}, {kwargs}')
-            else:
-                try:
-                    with open(filename, 'w') as file:
-                        file.write(f'{my_func.__name__} ok')
-                except Exception as e:
-                    with open(filename, 'w') as file:
-                        file.write(f'{my_func.__name__} error: {e}. Inputs: {args}, {kwargs}')
-            return result
-        return wrapper
-    return decorator
+    def wrapper(func: Any) -> Any:
+        def inner(*args: Any, **kwargs: Any) -> Any:
+            try:
+                result = func(*args, **kwargs)
+                if filename is None:
+                    print(f"{func.__name__} ok")
+                else:
+                    if len(filename) > 0:
+                        path_to_file = os.path.join(os.path.dirname(__file__), "../logs", filename)
+                        with open(path_to_file, "w", encoding="utf-8") as file:
+                            file.write(f"{func.__name__} ok")
+                return result
+            except Exception as e:
+                if filename is None:
+                    print(f"{func.__name__} error: {e}. Inputs: ({args}), {kwargs}")
+                else:
+                    path_to_file = os.path.join(os.path.dirname(__file__), "../logs", filename)
+                    with open(path_to_file, "w", encoding="utf-8") as file:
+                        file.write(f"{func.__name__} error: {e}. Inputs: ({args}), {kwargs}")
+        return inner
+    return wrapper
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,51 +1,61 @@
+import tempfile
+
 from src.decorators import log
 
 
-@log()
+@log(None)
 def my_function(x, y):
-    return x / y
+    # raise ValueError("Something went wrong")
+    return x + y
 
 
-def test_log_print(capsys):
-    my_function(10, 5)
+def test_log_ok(capsys):
+
+    @log()
+    def func(x, y):
+        return x + y
+
+    result = func(1, 2)
+    assert result == 3
+
+
+def test_log_ok_file_log(capsys):
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        log_file_path = tmp_file.name
+
+    @log(filename=log_file_path)
+    def func(x, y):
+        return x + y
+
+    func(1, 2)
+
+    with open(log_file_path, "r", encoding="utf-8") as file:
+        logs = file.read()
+
+    assert "func ok" in logs
+
+
+def test_log_exception(capsys):
+
+    @log()
+    def func(x, y):
+
+        return x + y
+
+    func(1, "2")
     captured = capsys.readouterr()
-    expected_output = (
-        'my_function started\n'
-        'my_function ok\n'
-        'my_function finished\n'
-    )
-    assert captured.out == expected_output
+    assert "func error: unsupported operand type(s) for +: 'int' and 'str'. Inputs: ((1, '2')), {}\n" in captured.out
 
 
-# def test_log_print_try(capsys):
-#     my_function(10, 0)
-#     captured = capsys.readouterr()
-#     expected_output = (
-#         'my_function started\n'
-#         'my_function error: division by zero. Inputs: (10, 0), {}\n'
-#     )
-#     assert captured.out == expected_output
+def test_log_exception_file_log():
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        log_file_path = tmp_file.name
 
+    @log(filename=log_file_path)
+    def func(x, y):
+        return x + y
 
-def test_log_print_fail(tmp_path):
-    log_file = tmp_path / "test_output.txt"
-
-    @log(log_file)
-    def my_function(x, y):
-        return x / y
-    my_function(10, 5)
-    with open(log_file, 'r') as f:
-        content = f.read()
-    assert content == 'my_function ok'
-
-
-# def test_log_print_fail_try(tmp_path):
-#     log_file = tmp_path / "test_output.txt"
-#
-#     @log(log_file)
-#     def my_function(x, y):
-#         return x / y
-#     my_function(10, 0)
-#     with open(log_file, 'r') as f:
-#         content = f.read()
-#     assert content == 'my_function error: division by zero. Inputs: (10, 0), {}'
+    func(1, "2")
+    with open(log_file_path, "r", encoding="utf-8") as file:
+        logs = file.read()
+    assert "func error: unsupported operand type(s) for +: 'int' and 'str'. Inputs: ((1, '2')), {}" in logs
