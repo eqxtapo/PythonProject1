688ffeafaa91bcbb3618531df55d700ccdde3fe4
unknown <extapo@yandex>
2025-01-21 14:15:44 +0700

    add logging

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,28 @@
+import logging
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+rel_log_file_path = os.path.join(current_dir, "../logs/masks.log")
+abs_log_file_path = os.path.abspath(rel_log_file_path)
+logger = logging.getLogger("masks")
+file_handler = logging.FileHandler(abs_log_file_path, 'w', encoding="utf-8")
+file_formatter = logging.Formatter('%(asctime)s - %(filename)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+
 def get_mask_card_number(card_number: str) -> str:
     """ Функция маскирует номер карты, заменяя 7-12 цифры на '*' """
-    return card_number[0:4] + " " + card_number[4:6] + "** ****" + " " + card_number[-4:]
+    logger.info("Начали маскировку карты")
+    mask_card_number = card_number[0:4] + " " + card_number[4:6] + "** ****" + " " + card_number[-4:]
+    logger.info("Маскировка карты закончена")
+    return mask_card_number
 
 
 def get_mask_account(accaunt_number: str) -> str:
     """ Функция маскирует номер аккаунта, оставляя последние 4 цифры"""
-    return "**" + accaunt_number[-4:]
+    logger.info("Начали маскировку номера счета")
+    mask_account = "**" + accaunt_number[-4:]
+    logger.info("Маскировка номера счета закончена")
+    return mask_account
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,16 @@
 import json
+import logging
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+rel_log_file_path = os.path.join(current_dir, "../logs/utils.log")
+abs_log_file_path = os.path.abspath(rel_log_file_path)
+logger = logging.getLogger("utils")
+file_handler = logging.FileHandler(abs_log_file_path, "w", encoding="utf-8")
+file_formatter = logging.Formatter('%(asctime)s - %(filename)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
 
 
 def transaction_returner(path: str) -> list[dict]:
@@ -6,9 +18,11 @@ def transaction_returner(path: str) -> list[dict]:
     Если файл пустой, содержит не список или не найден, функция возвращает пустой список."""
     returned_list = []
     try:
+        logger.info("Начал выгрузку с файла")
         with open(path, encoding="utf-8") as json_file:
             content = json.load(json_file)
             if isinstance(content, list):
                 returned_list = content
+        logger.info("Окончил выгрузку с файла")
     finally:
         return returned_list
