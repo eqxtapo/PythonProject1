352bbeadfd069eb3106885d295b124cc76675b22
unknown <extapo@yandex>
2025-01-25 13:39:41 +0700

    add new func with tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,3 +13,11 @@ def dict():
         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+
+@pytest.fixture
+def test_dict_list_with_description():
+    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364', 'description': 'Перевод'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572', 'description': 'Платеж'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689', 'description': 'Перевод'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441', 'description': 'Платеж'}]
